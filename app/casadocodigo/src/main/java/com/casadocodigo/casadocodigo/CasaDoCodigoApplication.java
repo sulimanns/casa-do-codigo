610a4d70c729980347db9fc410dafd316a780548
Rafael Sulimann <sulimannrafael@gmail.com>
2023-12-20 21:06:02 -0300

    adicionar consul discovery

--- app/casadocodigo/src/main/java/com/casadocodigo/casadocodigo/CasaDoCodigoApplication.java
+++ app/casadocodigo/src/main/java/com/casadocodigo/casadocodigo/CasaDoCodigoApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import lombok.extern.log4j.Log4j2;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableDiscoveryClient
 @Log4j2
 public class CasaDoCodigoApplication implements CommandLineRunner{
 
